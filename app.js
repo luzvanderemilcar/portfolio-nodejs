979d4a6b475b76abdaa1c0a50468c05d4df23295
Luzvander EMILCAR <luzvanderemilcar@gmail.com>
2025-01-11 11:55:06 -0500

    Set up route for the main pages

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-require('dotenv').config();
-const express = require('express');
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -13,9 +15,23 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.render("pages/index", {title: "welcome", tagline: "positive", firstHeading: "Welcome aboard" });
+});
+
+app.get('/projects', (req, res) => { 
+
+let webApps = [
+	{id: 1, title:"Javascript multitimer" , githubLink : ""}, 
+	{id: 2, title:"Javascript BMI calculator" , githubLink : ""}, 
+	{id: 3, title:"Javascript calculatorr" , githubLink : ""}
+]; 
+  res.render("pages/projects", {title: "projects", tagline: "Projects", firstHeading: "Find out interesting projects", webApps : webApps});
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get('/about', (req, res) => { res.render("pages/about", {title:  "about", tagline: "About", firstHeading: "About me"});
+});
 
+
+// Configuration server port
+
+app.listen(port, () => { console.log(`Server listening on port ${port}`)
+})
