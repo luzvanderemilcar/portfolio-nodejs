0e1c8b8a2698341fa993f53da6aa8fb36747fa11
Luzvander EMILCAR <luzvanderemilcar@gmail.com>
2025-01-13 12:35:39 -0500

    Add some custom styles, craete route for patch request on specific project

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ import bodyParser from "body-parser";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
+console.log(process.env.PORT);
+
 let webApps = [
-        {id: 1, title:"Javascript multitimer" , githubLink : ""},
-        {id: 2, title:"Javascript BMI calculator" , githubLink : ""},
-        {id: 3, title:"Javascript calculatorr" , githubLink : ""}
+        {id: 1, title:"Javascript multitimer" , description:  "A timer with the ability to handle multiple timer simultaneously", summary:  "I was not planning to do a timer at the first place  but as I was developing, and was adding more features, it was ckear that it was important. I came across a lot of timer application and I think, it is the pinch of salt they needed", githubLink : "", imageUrl: ""},
+        {id: 2, title:"Javascript BMI calculator", description:  "A javascript Body Mass Index (BMI)  calculator that produce a summary and provide good advice accordingly", summary:  "I was not planning to do  at the first place  but as I was developing, and was adding more features, it was clear that it was important. I came across a lot of calculator application and I think, it is the pinch of salt they needed", githubLink : "", imageUrl: ""},
+        {id: 3, title:"Javascript calculator" ,  description:  "A javascript calculator developed with React", summary:  "I was not planning to do  at the first place  but as I was developing, and was adding more features, it was clear that it was important. I came across a lot of calculator application and I think, it is the pinch of salt they needed", githubLink : "", imageUrl: ""}
 ];
 
 // set view folder location  
@@ -28,17 +30,48 @@ app.get('/', (req, res) => {
 app.get('/projects', (req, res) => { 
 
   res.render("pages/projects", {title: "projects", tagline: "Projects", firstHeading: "Find out interesting projects", webApps : webApps});
-})
+});
+
+app.post("/projects", (req, res) => {
+        let {title, descrition,  summary, codeLink } = req.body;
+        let id = webApps.at(-1).id +1;
+
+        let newApp = {id, title, descrition, summary, codeLink};
+         webApps.push(newApp);
+        res.redirect("/projects/" + id, );
+
+});
+
+app.get("/projects/add", (req, res) => {
+ res.render("pages/addProject", {title: "add project", tagline: "Add to projects", "firstHeading" : "Add a new project to the store of projects"});
+});
 
 // route for specific project 
-app.get("/projects/:id", (req, res) => {
+app.route("/projects/:id")
+  .get((req, res) => {
 	let {id:projectId} = req.params ;
-	console.log(projectId)
 
-	let project = webApps.find(app => app.id === projectId);
+	let project = webApps.find(app => app.id == projectId);
 
-res.render("pages/projectItem", project) //.status("code": 200, "message: "okay");
+res.render("pages/projectItem", {title: "project item", project: project }) //.status("code": 200, "message: "okay");
+})
+
+ .patch((req, res) => {
+        let {id:projectId} = req.params ;
+	let matchIndex ;
+        let project = webApps.find((app, index) => {
+
+		if ( app.id == projectId) {
+			matchIndex = index;
+			return true;
+		}
+
+	});
+		console.log(matchIndex, project);
+		res.sendStatus(200);
 });
+
+
 app.get('/about', (req, res) => { res.render("pages/about", {title:  "about", tagline: "About", firstHeading: "About me"});
 });
 
